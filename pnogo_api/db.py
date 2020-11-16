8f45ff15b6f5eb21999ebb5a24fbba90cee2b548
Marco Fincato <marcofincato777@gmail.com>
2020-11-16 23:57:56 +0100

    Creating db at startup if not found

--- pnogo_api/db.py
+++ pnogo_api/db.py
@@ -1,37 +1,21 @@
+import os
 import sqlite3
 
-import click
 from flask import current_app, g
-from flask.cli import with_appcontext
 
 
 def init_app(app):
     app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
-
-
-def init_db():
-    db = get_db()
-
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-
-
-@click.command('init-db')
-@with_appcontext
-def init_db_command():
-    """Clear the existing data and create new tables."""
-    init_db()
-    click.echo('Initialized the database.')
 
 
 def get_db():
+    dbpath = current_app.config['DATABASE']
     if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
+        create = not os.path.isfile(dbpath)
+        g.db = sqlite3.connect(dbpath, detect_types=sqlite3.PARSE_DECLTYPES)
+        if create:
+            with current_app.open_resource('schema.sql') as f:
+                g.db.executescript(f.read().decode('utf8'))
 
     return g.db
 
