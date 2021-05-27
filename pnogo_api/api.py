6dc0feb1c6f87dad6cddaeb8ffa127da30afa2dd
Marco Fincato <marcofincato777@gmail.com>
2021-05-27 18:41:15 +0200

    Added file upload using POST

--- pnogo_api/api.py
+++ pnogo_api/api.py
@@ -1,6 +1,8 @@
 from flask import (
     Blueprint, request, send_from_directory, current_app, abort
 )
+from werkzeug.utils import secure_filename
+import os
 
 from pnogo_api.auth import require_app_key
 from pnogo_api.db import query_db, execute_db
@@ -59,3 +61,35 @@ def update():
         "added": res[0],
         "removed": res[1],
     }
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@bp.route('/addpnogo', methods=['GET', 'POST'])
+@require_app_key
+def addpnogo():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'picture' not in request.files:
+            return 'morte: no file'
+        file = request.files['picture']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            return 'morte: no file name'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(current_app.config['PONGHI'], filename))
+            return 'done!'
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=picture>
+      <input type=submit value=Upload>
+    </form>
+    '''
