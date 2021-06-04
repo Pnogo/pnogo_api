ec06ed6217dc00957277c3f189f264f03d2ddf24
Marco Fincato <marcofincato777@gmail.com>
2021-06-05 00:41:49 +0200

    Bump version and added new dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pnogo_api',
-    version='0.3.8',
+    version='0.4.0',
     url='api.pnogo.ml',
     license='',
     author='marc0777',
@@ -15,5 +15,6 @@ setup(
     install_requires=[
         'flask',
         'flask-cors',
+        'Pillow',
     ],
 )
