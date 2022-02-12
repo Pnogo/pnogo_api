fb74c9d60e820e19d655da24e32d113f8588a4e1
Marco Fincato <marcofincato777@gmail.com>
2022-02-13 00:15:58 +0100

    Bump version and update schema

--- pnogo_api/schema.sql
+++ pnogo_api/schema.sql
@@ -5,13 +5,13 @@ CREATE TABLE "pictures" (
 	"points"	INTEGER DEFAULT 0,
 	"sent"	INTEGER DEFAULT 0,
 	"daily_date"	TEXT,
-	"cndr_id"	INTEGER NOT NULL DEFAULT 1,
+	"cndr_id"	INTEGER NOT NULL DEFAULT 0,
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
 CREATE TABLE "cndr" (
 	"id"	INTEGER,
-	"name"	TEXT,
+	"name"	TEXT NOT NULL UNIQUE,
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
